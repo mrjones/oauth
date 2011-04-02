1aa707473d35265591ebee3859c17c4c3c19b74e
Matt Jones <jonesmr+github@gmail.com>
2011-04-02 19:58:28 +0000

    request working, invalid signature

--- oauth.go
+++ oauth.go
@@ -1,6 +1,12 @@
 package oauth
 
 import (
+       "crypto/hmac"
+       "encoding/base64"
+       "fmt"
+       "http"
+       "io/ioutil"
+       "log"
        "os"
        "rand"
        "sort"
@@ -32,7 +38,7 @@ type UnauthorizedToken struct {
 func baseParams() *OrderedParams {
   params := NewOrderedParams()
   params.Add("oauth_version", OAUTH_VERSION)
-  params.Add("oauth_signature_mathod", SIGNATURE_METHOD)
+  params.Add("oauth_signature_method", SIGNATURE_METHOD)
   params.Add("oauth_timestamp", strconv.Itoa64(time.Seconds()))
   params.Add("oauth_nonce", strconv.Itoa64(rand.Int63()))
   
@@ -44,11 +50,106 @@ func (c *Consumer) GetRequestToken() (*UnauthorizedToken, os.Error) {
      for key, value := range c.AdditionalParams {
          params.Add(key, value)
      }
+     params.Add("oauth_consumer_key", c.ConsumerKey)
 
      params.Add("oauth_callback", c.CallbackUrl)
 
+     key := escape(c.ConsumerSecret + "&") // no token secret when requesting
 
+     req := c.requestString("GET", c.RequestTokenUrl, params)
+     fmt.Println(req)
+     signature := digest(req, key)
+     escapedsignature := http.URLEscape(signature)
+     params.Add("oauth_signature", escapedsignature)
+
+     fmt.Printf("%s \n-> %s \n-> %s\n", req, signature, escapedsignature)
+
+     resp, err := get(c.RequestTokenUrl, params)
+
+     if err != nil {
+        log.Fatal(err)
+        return nil, err
+     }
+     defer resp.Body.Close()
+
+     contents, err := ioutil.ReadAll(resp.Body)
+     if err != nil {
+        return nil, err
+     }
+     fmt.Println(string(contents))
+     
      return nil, nil
+}
+
+func digest(message string, key string) string {
+     hashfun := hmac.NewSHA1([]byte(key))
+     hashfun.Write([]byte(message))
+     rawsignature := hashfun.Sum()
+     base64signature := make([]byte, base64.StdEncoding.EncodedLen(len(rawsignature)))
+     base64.StdEncoding.Encode(base64signature, rawsignature)
+     return string(base64signature)
+}
+
+func escape(input string) string {
+     return http.URLEscape(input)
+}
+
+func (c *Consumer) requestString(method string, url string, params *OrderedParams) string {
+     result := method + "&" + escape(url)
+     for pos, key := range params.Keys() {
+         if pos == 0 {
+            result += "&"
+         } else {
+            result += escape("&")
+         }
+         result += escape(fmt.Sprintf("%s=%s", key, params.Get(key)))
+     }
+     return result
+}
+
+func get(url string, params *OrderedParams) (*http.Response, os.Error) {
+     var req http.Request
+     req.Method = "GET"
+     req.Header = http.Header{}
+     parsedurl, err := http.ParseURL(url)
+     if err != nil {
+        return nil, err
+     }
+     req.URL = parsedurl
+
+     authhdr := "OAuth "
+     for pos, key := range params.Keys() {
+         if pos > 0 {
+            authhdr += ",\n    "
+         }
+         authhdr += key + "=\"" + params.Get(key) + "\""
+     }
+     fmt.Println("AUTH-HDR: " + authhdr)
+     req.Header.Add("Authorization", authhdr)
+
+     client := &http.Client{}
+     return client.Do(&req)
+
+//     addr := req.URL.Host
+//     conn, err := tls.Dial("tcp", "", addr, nil)
+//     if err != nil {
+//        return nil, err
+//     }
+//
+//     err = req.Write(conn)
+//     if err != nil {
+//        conn.Close()
+//        return nil, err
+//     }
+//
+//     reader := bufio.NewReader(conn)
+//     resp, err := http.ReadResponse(reader, req.Method)
+//     if err != nil {
+//        conn.Close()
+//        return nil, err
+//     }
+
+//     return resp, nil
 }
 
 //
@@ -63,7 +164,7 @@ type OrderedParams struct {
 func NewOrderedParams() *OrderedParams {
      return &OrderedParams {
        allParams: make(map[string]string),
-       keyOrdering: make([]string, 1),
+       keyOrdering: make([]string, 0),
      }
 }
 
@@ -77,8 +178,11 @@ func (o *OrderedParams) Keys() []string {
 }
 
 func (o* OrderedParams) Add(key, value string) {
+     if key == "" {
+        log.Fatal("BAD KEY")
+     }
      o.allParams[key] = value
-     _ = append(o.keyOrdering, key)
+     o.keyOrdering = append(o.keyOrdering, key)
 }
 
 func (o *OrderedParams) Len() int {
