fe5f876e3a46da199b93ae983366455bc4203f31
Matt Jones <jonesmr+github@gmail.com>
2012-04-01 17:19:26 +0000

    twitter example

---
+++ examples/twitter/twitter.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/mrjones/oauth"
+)
+
+func Usage() {
+	fmt.Println("Usage:")
+	fmt.Print("go run examples/twitter/twitter.go")
+	fmt.Print("  --consumerkey <consumerkey>")
+	fmt.Println("  --consumersecret <consumersecret>")
+	fmt.Println("")
+	fmt.Println("In order to get your consumerkey and consumersecret, you must register an 'app' at twitter.com:")
+	fmt.Println("https://dev.twitter.com/apps/new")
+}
+
+func main() {
+	var consumerKey *string = flag.String(
+		"consumerkey",
+		"",
+		"Consumer Key from Twitter. See: https://dev.twitter.com/apps/new")
+
+	var consumerSecret *string = flag.String(
+		"consumersecret",
+		"",
+		"Consumer Secret from Twitter. See: https://dev.twitter.com/apps/new")
+
+	flag.Parse()
+
+	if len(*consumerKey) == 0 || len(*consumerSecret) == 0 {
+		fmt.Println("You must set the --consumerkey and --consumersecret flags.")
+		fmt.Println("---");
+		Usage();
+		os.Exit(1)
+	}
+
+	c := oauth.NewConsumer(
+		*consumerKey,
+		*consumerSecret,
+		oauth.ServiceProvider{
+			RequestTokenUrl:   "http://api.twitter.com/oauth/request_token",
+			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+		})
+
+	c.Debug(true)
+
+	requestToken, url, err := c.GetRequestTokenAndUrl("oob")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("(1) Go to: " + url)
+	fmt.Println("(2) Grant access, you should get back a verification code.")
+	fmt.Println("(3) Enter that verification code here: ")
+
+	verificationCode := ""
+	fmt.Scanln(&verificationCode)
+
+	accessToken, err := c.AuthorizeToken(requestToken, verificationCode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := c.Get(
+	  "http://api.twitter.com/1/statuses/home_timeline.json",
+	  map[string]string{"count": "1"},
+		accessToken)
+
+// Uncomment this to post a status update
+//	response, err := c.Post(
+//		"http://api.twitter.com/1/statuses/update.json",
+//		"",
+//		map[string]string{
+//			"key": "YgV7Rq8CyfvvfANEbFxZA",
+//			"status": "Test post via the API using Go -- http://golang.org/",
+//		},
+//		accessToken)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	bits, err := ioutil.ReadAll(response.Body)
+	fmt.Println("The newest item in your home timeline is: " + string(bits))
+}
