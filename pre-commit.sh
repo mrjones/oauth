87cdf5549b2a64f89c9618e3aca50e5d6ac98524
Matt Jones <jonesmr+github@gmail.com>
2012-11-21 17:41:58 +0000

    a bug, to make sure pre-commit works

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 # ln -s pre-commit.sh .git/hooks/pre-commit
 go test *.go
+if [[ $RESULT != 0 ]]; then exit $RESULT; fi
+
 go fmt *.go
 for e in $(ls examples); do 
     go build examples/$e/*.go
+    if [[ $RESULT != 0 ]]; then exit $RESULT; fi
     go fmt examples/$e/*.go
 done
+
+exit 0
