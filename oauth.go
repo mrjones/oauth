c5497404705deb6d41971469e8574952ae4e96fa
Forestmb <forestmb@gmail.com>
2013-12-07 16:31:08 -0500

    Cleaning up doc formatting.

--- oauth.go
+++ oauth.go
@@ -2,34 +2,34 @@
 // See http://www.oauth.net and RFC 5849
 //
 // There are typically three parties involved in an OAuth exchange:
-// (1) The "Service Provider" (e.g. Google, Twitter, NetFlix) who operates the
-//     service where the data resides.
-// (2) The "End User" who owns that data, and wants to grant access to a third-party.
-// (3) That third-party who wants access to the data (after first being authorized by the
-//     user). This third-party is referred to as the "Consumer" in OAuth terminology.
+//      (1) The "Service Provider" (e.g. Google, Twitter, NetFlix) who operates the
+//          service where the data resides.
+//      (2) The "End User" who owns that data, and wants to grant access to a third-party.
+//      (3) That third-party who wants access to the data (after first being authorized by the
+//          user). This third-party is referred to as the "Consumer" in OAuth terminology.
 //
 // This library is designed to help implement the third-party consumer by handling the
 // low-level authentication tasks, and allowing for authenticated requests to the
 // service provider on behalf of the user.
 //
 // Caveats:
-// - Currently only supports HMAC-SHA1 signatures.
-// - Currently only supports OAuth 1.0
+//      - Currently only supports HMAC-SHA1 signatures.
+//      - Currently only supports OAuth 1.0
 //
 // Overview of how to use this library:
-// (1) First create a new Consumer instance with the NewConsumer function
-// (2) Get a RequestToken, and "authorization url" from GetRequestTokenAndUrl()
-// (3) Save the RequestToken, you will need it again in step 6.
-// (4) Redirect the user to the "authorization url" from step 2, where they will authorize
-//     your access to the service provider.
-// (5) Wait. You will be called back on the CallbackUrl that you provide, and you
-//     will recieve a "verification code".
-// (6) Call AuthorizeToken() with the RequestToken from step 2 and the "verification code"
-//     from step 5.
-// (7) You will get back an AccessToken.  Save this for as long as you need access to
-//     the user's data, and treat it like a password; it is a secret.
-// (8) You can now throw away the RequestToken from step 2, it is no longer necessary.
-// (9) Call "Get" using the AccessToken from step 7 to access protected resources.
+//      (1) First create a new Consumer instance with the NewConsumer function
+//      (2) Get a RequestToken, and "authorization url" from GetRequestTokenAndUrl()
+//      (3) Save the RequestToken, you will need it again in step 6.
+//      (4) Redirect the user to the "authorization url" from step 2, where they will authorize
+//          your access to the service provider.
+//      (5) Wait. You will be called back on the CallbackUrl that you provide, and you
+//          will recieve a "verification code".
+//      (6) Call AuthorizeToken() with the RequestToken from step 2 and the "verification code"
+//          from step 5.
+//      (7) You will get back an AccessToken.  Save this for as long as you need access to
+//          the user's data, and treat it like a password; it is a secret.
+//      (8) You can now throw away the RequestToken from step 2, it is no longer necessary.
+//      (9) Call "Get" using the AccessToken from step 7 to access protected resources.
 package oauth
 
 import (
@@ -89,23 +89,23 @@ const (
 // You usually find all of these URLs by reading the documentation for the service
 // that you're trying to connect to.
 // Some common examples are:
-// (1) Google, standard APIs:
-//     http://code.google.com/apis/accounts/docs/OAuth_ref.html
-//     - RequestTokenUrl:   https://www.google.com/accounts/OAuthGetRequestToken
-//     - AuthorizeTokenUrl: https://www.google.com/accounts/OAuthAuthorizeToken
-//     - AccessTokenUrl:    https://www.google.com/accounts/OAuthGetAccessToken
-//     Note: Some Google APIs (for example, Google Latitude) use different values for
-//     one or more of those URLs.
-// (2) Twitter API:
-//     http://dev.twitter.com/pages/auth
-//     - RequestTokenUrl:   http://api.twitter.com/oauth/request_token
-//     - AuthorizeTokenUrl: https://api.twitter.com/oauth/authorize
-//     - AccessTokenUrl:    https://api.twitter.com/oauth/access_token
-// (3) NetFlix API:
-//     http://developer.netflix.com/docs/Security
-//     - RequestTokenUrl:   http://api.netflix.com/oauth/request_token
-//     - AuthroizeTokenUrl: https://api-user.netflix.com/oauth/login
-//     - AccessTokenUrl:    http://api.netflix.com/oauth/access_token
+//      (1) Google, standard APIs:
+//          http://code.google.com/apis/accounts/docs/OAuth_ref.html
+//          - RequestTokenUrl:   https://www.google.com/accounts/OAuthGetRequestToken
+//          - AuthorizeTokenUrl: https://www.google.com/accounts/OAuthAuthorizeToken
+//          - AccessTokenUrl:    https://www.google.com/accounts/OAuthGetAccessToken
+//          Note: Some Google APIs (for example, Google Latitude) use different values for
+//          one or more of those URLs.
+//      (2) Twitter API:
+//          http://dev.twitter.com/pages/auth
+//          - RequestTokenUrl:   http://api.twitter.com/oauth/request_token
+//          - AuthorizeTokenUrl: https://api.twitter.com/oauth/authorize
+//          - AccessTokenUrl:    https://api.twitter.com/oauth/access_token
+//      (3) NetFlix API:
+//          http://developer.netflix.com/docs/Security
+//          - RequestTokenUrl:   http://api.netflix.com/oauth/request_token
+//          - AuthroizeTokenUrl: https://api-user.netflix.com/oauth/login
+//          - AccessTokenUrl:    http://api.netflix.com/oauth/access_token
 type ServiceProvider struct {
 	RequestTokenUrl   string
 	AuthorizeTokenUrl string
@@ -149,11 +149,11 @@ type Consumer struct {
 }
 
 // Creates a new Consumer instance.
-// - consumerKey and consumerSecret
-//   values you should obtain from the ServiceProvider when you register your application.
+//      - consumerKey and consumerSecret:
+//        values you should obtain from the ServiceProvider when you register your application.
 //
-// - serviceProvider:
-//   see the documentation for ServiceProvider for how to create this.
+//      - serviceProvider:
+//        see the documentation for ServiceProvider for how to create this.
 //
 func NewConsumer(consumerKey string, consumerSecret string,
 	serviceProvider ServiceProvider) *Consumer {
@@ -173,29 +173,29 @@ func NewConsumer(consumerKey string, consumerSecret string,
 }
 
 // Kicks off the OAuth authorization process.
-// - callbackURL
-//   Authorizing a token *requires* redirecting to the service provider. This is the URL
-//   which the service provider will redirect the user back to after that authorization
-//   is completed. The service provider will pass back a verification code which is
-//   necessary to complete the rest of the process (in AuthorizeToken).
-//   Notes on callbackURL:
-//   - Some (all?) service providers allow for setting "oob" (for out-of-band) as a callback
-//     url.  If this is set the service provider will present the verification code directly
-//     to the user, and you must provide a place for them to copy-and-paste it into.
-//   - Otherwise, the user will be redirected to callbackUrl in the browser, and will
-//     append a "oauth_verifier=<verifier>" parameter.
+//      - callbackUrl:
+//        Authorizing a token *requires* redirecting to the service provider. This is the URL
+//        which the service provider will redirect the user back to after that authorization
+//        is completed. The service provider will pass back a verification code which is
+//        necessary to complete the rest of the process (in AuthorizeToken).
+//        Notes on callbackUrl:
+//          - Some (all?) service providers allow for setting "oob" (for out-of-band) as a callback
+//            url.  If this is set the service provider will present the verification code directly
+//            to the user, and you must provide a place for them to copy-and-paste it into.
+//          - Otherwise, the user will be redirected to callbackUrl in the browser, and will
+//            append a "oauth_verifier=<verifier>" parameter.
 //
 // This function returns:
-// - rtoken:
-//   A temporary RequestToken, used during the authorization process.  You must save this
-//   since it will be necessary later in the process when calling AuthorizeToken().
+//      - rtoken:
+//        A temporary RequestToken, used during the authorization process.  You must save this
+//        since it will be necessary later in the process when calling AuthorizeToken().
 //
-// - url:
-//   A URL that you should redirect the user to in order that they may authorize you to
-//   the service provider.
+//      - url:
+//        A URL that you should redirect the user to in order that they may authorize you to
+//        the service provider.
 //
-// - err:
-//   Set only if there was an error, nil otherwise.
+//      - err:
+//        Set only if there was an error, nil otherwise.
 func (c *Consumer) GetRequestTokenAndUrl(callbackUrl string) (rtoken *RequestToken, loginUrl string, err error) {
 	params := c.baseParams(c.consumerKey, c.AdditionalParams)
 	params.Add(CALLBACK_PARAM, callbackUrl)
@@ -226,21 +226,21 @@ func (c *Consumer) GetRequestTokenAndUrl(callbackUrl string) (rtoken *RequestTok
 
 // After the user has authorized you to the service provider, use this method to turn
 // your temporary RequestToken into a permanent AccessToken. You must pass in two values:
-// - rtoken:
-//   The RequestToken returned from GetRequestTokenAndUrl()
+//      - rtoken:
+//        The RequestToken returned from GetRequestTokenAndUrl()
 //
-// - verificationCode:
-//   The string which passed back from the server, either as the oauth_verifier
-//   query param appended to callbackUrl *OR* a string manually entered by the user
-//   if callbackUrl is "oob"
+//      - verificationCode:
+//        The string which passed back from the server, either as the oauth_verifier
+//        query param appended to callbackUrl *OR* a string manually entered by the user
+//        if callbackUrl is "oob"
 //
 // It will return:
-// - atoken:
-//   A permanent AccessToken which can be used to access the user's data (until it is
-//   revoked by the user or the service provider).
+//      - atoken:
+//        A permanent AccessToken which can be used to access the user's data (until it is
+//        revoked by the user or the service provider).
 //
-// - err:
-//   Set only if there was an error, nil otherwise.
+//      - err:
+//        Set only if there was an error, nil otherwise.
 func (c *Consumer) AuthorizeToken(rtoken *RequestToken, verificationCode string) (atoken *AccessToken, err error) {
 	params := map[string]string{
 		VERIFIER_PARAM: verificationCode,
@@ -259,18 +259,17 @@ func (c *Consumer) AuthorizeToken(rtoken *RequestToken, verificationCode string)
 //
 // See http://oauth.googlecode.com/svn/spec/ext/session/1.0/drafts/1/spec.html
 // for more information.
-//
-// - accessToken:
-//   The AccessToken returned from AuthorizeToken()
+//      - accessToken:
+//        The AccessToken returned from AuthorizeToken()
 //
 // It will return:
-// - atoken:
-//   An AccessToken which can be used to access the user's data (until it is
-//   revoked by the user or the service provider).
+//      - atoken:
+//        An AccessToken which can be used to access the user's data (until it is
+//        revoked by the user or the service provider).
 //
-// - err:
-//   Set if accessToken does not contain the SESSION_HANDLE_PARAM needed to 
-//   refresh the token, or if an error occurred when making the request.
+//      - err:
+//        Set if accessToken does not contain the SESSION_HANDLE_PARAM needed to 
+//        refresh the token, or if an error occurred when making the request.
 func (c *Consumer) RefreshToken(accessToken *AccessToken) (atoken *AccessToken, err error) {
 	params := make(map[string]string)
 	sessionHandle, ok := accessToken.AdditionalData[SESSION_HANDLE_PARAM]
@@ -284,18 +283,19 @@ func (c *Consumer) RefreshToken(accessToken *AccessToken) (atoken *AccessToken, 
 }
 
 // Use the service provider to obtain an AccessToken for a given session
-// - params:
-//   The access token request paramters.
-// - secret:
-//   Secret key to use when signing the access token request.
+//      - params:
+//        The access token request paramters.
+//
+//      - secret:
+//        Secret key to use when signing the access token request.
 //
 // It will return:
-// - atoken
-//   An AccessToken which can be used to access the user's data (until it is
-//   revoked by the user or the service provider).
+//      - atoken
+//        An AccessToken which can be used to access the user's data (until it is
+//        revoked by the user or the service provider).
 //
-// - err:
-//   Set only if there was an error, nil otherwise.
+//      - err:
+//        Set only if there was an error, nil otherwise.
 func (c *Consumer) makeAccessTokenRequest(params map[string]string, secret string) (atoken *AccessToken, err error) {
 	orderedParams := c.baseParams(c.consumerKey, c.AdditionalParams)
 	for key, value := range params {
@@ -313,22 +313,22 @@ func (c *Consumer) makeAccessTokenRequest(params map[string]string, secret strin
 	return parseAccessToken(*resp)
 }
 
-// Executes an HTTP Get,, authorized via the AccessToken.
-// - url:
-//   The base url, without any query params, which is being accessed
+// Executes an HTTP Get, authorized via the AccessToken.
+//      - url:
+//        The base url, without any query params, which is being accessed
 //
-// - userParams:
-//   Any key=value params to be included in the query string
+//      - userParams:
+//        Any key=value params to be included in the query string
 //
-// - token:
-//   The AccessToken returned by AuthorizeToken()
+//      - token:
+//        The AccessToken returned by AuthorizeToken()
 //
 // This method returns:
-// - resp:
-//   The HTTP Response resulting from making this request.
+//      - resp:
+//        The HTTP Response resulting from making this request.
 //
-// - err:
-//   Set only if there was an error, nil otherwise.
+//      - err:
+//        Set only if there was an error, nil otherwise.
 func (c *Consumer) Get(url string, userParams map[string]string, token *AccessToken) (resp *http.Response, err error) {
 	return c.makeAuthorizedRequest("GET", url, LOC_URL, "", userParams, token)
 }
@@ -479,9 +479,16 @@ func (c *Consumer) makeKey(tokenSecret string) string {
 }
 
 // Obtains an AccessToken from the response of a service provider.
-// - data:
-//   The response body.
-func parseAccessToken(data string) (*AccessToken, error) {
+//      - data:
+//        The response body.
+//
+// This method returns:
+//      - atoken:
+//        The AccessToken generated from the response body.
+//
+//      - err:
+//        Set if an AccessToken could not be parsed from the given input.
+func parseAccessToken(data string) (atoken *AccessToken, err error) {
 	parts, err := url.ParseQuery(data)
 	if err != nil {
 		return nil, err
